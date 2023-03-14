9bd3a64b24d5e229f601319f6d3295fee98781bf
AndWa <andrej.saric.du@gmail.com>
2023-03-14 16:41:26 +0100

    Add Github workflow

--- contract/build.sh
+++ contract/build.sh
@@ -3,4 +3,4 @@
 echo ">> Building contract"
 
 rustup target add wasm32-unknown-unknown
-cargo build --all --target wasm32-unknown-unknown --release  && cp ./target/wasm32-unknown-unknown/release/axelar_auth_gateway.wasm ../out/axelar_auth_gateway.wasm
+cargo build --all --target wasm32-unknown-unknown --release  && rm -rf ../dist && mkdir ../dist && cp ./target/wasm32-unknown-unknown/release/axelar_auth_gateway.wasm ../dist/axelar_auth_gateway.wasm
--- near-axelar-contract-call-example/build.sh
+++ near-axelar-contract-call-example/build.sh
@@ -3,4 +3,4 @@
 echo ">> Building contract"
 
 rustup target add wasm32-unknown-unknown
-cargo build --all --target wasm32-unknown-unknown --release && cp ./target/wasm32-unknown-unknown/release/near_axelar_contract_call_example.wasm ../out/near_axelar_contract_call_example.wasm
+cargo build --all --target wasm32-unknown-unknown --release
